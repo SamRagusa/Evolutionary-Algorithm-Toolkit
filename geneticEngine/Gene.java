115a518529a11f1ff04a620289e67392295661eb
Sam Ragusa <Samragusa@gmail.com>
2016-10-04 16:13:48 -0400

    Initial Commit and Gene Class
    Finally getting around to putting some of the code I wrote this summer
    online!
    In this commit I added the Gene class.

---
+++ geneticEngine/Gene.java
@@ -0,0 +1,54 @@
+package geneticEngine;
+
+/**
+ * An abstract class that represents some characteristic of a Chromosome object.  
+ *
+ * @author Sam Ragusa
+ * @param <V> An object to store (and manipulate if appropriate) whatever information
+ * that the Gene object is supposed to store. 
+ */
+public abstract class Gene<V>
+{
+	/**
+	 * The information that the Gene stores.
+	 */
+	public V info;
+	
+	/**
+	 * A constructor to create a Gene containing random information.
+	 */
+	public Gene() 
+	{
+		setDataRandomly();
+	}
+	
+	/**
+	 * Gets and returns the instance variable info.
+	 * 
+	 * @return The object contained within the instance variable "info".
+	 */
+	public V getInfo()
+	{
+		return info;
+	}
+	
+	/**
+	 * Replace the information contained within the gene by new, random information.
+	 */
+	abstract public void mutate();
+
+	/**
+	 * Set the info contained within the Gene to random
+	 * values.  This is used when initializing the population.
+	 */
+	abstract public void setDataRandomly();
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public String toString()
+	{
+		return info.toString();
+	}
+	
+}
